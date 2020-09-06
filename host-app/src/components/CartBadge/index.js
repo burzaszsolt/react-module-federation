390b84e4fd0ab56dc36db9b5ba768c7718e166ec
Zsolt Burzas <zsolt.burzas@infinitaslearning.com>
2020-09-06 22:50:27 +0200

    set cart item number

--- host-app/src/components/CartBadge/index.js
+++ host-app/src/components/CartBadge/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
@@ -6,7 +6,11 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const CartBadge = ({ cart }) => {
   let history = useHistory();
-  const [cartNum] = useState(cart.length);
+  const [cartNum, setCartNum] = useState(cart.length);
+
+  useEffect(() => {
+    setCartNum(cart.length);
+  }, [cart]);
 
   const handleClick = () => history.push('/cart');
 
